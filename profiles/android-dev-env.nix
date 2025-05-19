008a24d9e1164c16e384b8c683fcb265833fb1b2
rencire <546296+rencire@users.noreply.github.com>
2025-05-19 00:42:11 -0700

    refactor: remove old comments

--- profiles/android-dev-env.nix
+++ profiles/android-dev-env.nix
@@ -3,7 +3,6 @@
   config,
   options,
   lib,
-  dmTypes,
   dmUtils,
   ...
 }:
@@ -17,15 +16,22 @@ let
   # - We start setting the preset override values at the minimum
   #   override level (i.e. 100).  This is to ensure that priority
   #   for the preset values are higher than the null priority value (default to 1000).
-  #   - For details on the null override priority level, see `mkPreset`
+  # - For details on the null override priority level, see `utils.nix#mkPreset`.
   consumerCfgPriority = 80;
+
   # Base module settings we want enabled, wheneve this profile is enabled
   basePreset = dmUtils.mkPreset 90 {
-    # android.enable = true;
+    android.enable = true;
     gradle.enable = true;
     languages.java.enable = true;
   };
   # Define module settings for each preset
+  # Consumer can set presets for common option values to pass to dependent modules.
+  #
+  # Note that each preset has a default override priority value applied to all values
+  # in its attribute set.
+  # - We can optionally modify the priority for each specific individual value also.
+  #    (See: utils.nix#mkPreset)
   presets = {
     "android-api-34" = dmUtils.mkPreset 100 {
       languages.java = {
@@ -34,39 +40,29 @@ let
       gradle = {
         version = "8.8";
       };
-      # android = {
-      #   enable = true;
-      #   platform.compileSdkVersion = "from preset 3";
-      #   platform.versions = [ "34" ];
-      #   androidGradlePlugin.version = "8.6.0";
-      #   buildTools.versions = [ "34.0.0" ];
-      #   cmake.versions = [ "3.22.1" ];
-      #   ndk.versions = [
-      #     "26.3.11579264"
-      #   ];
-      # };
+      android = {
+        platform.compileSdkVersion = "from preset 3";
+        platform.versions = [ "34" ];
+        androidGradlePlugin.version = "8.6.0";
+        buildTools.versions = [ "34.0.0" ];
+        cmake.versions = [ "3.22.1" ];
+        ndk.versions = [
+          "26.3.11579264"
+        ];
+      };
     };
   };
-
+  # Get list of selected presets specified by consumer of this profile
   selectedPresetsList = map (key: presets.${key}) cfg.presets;
-  # cfg that is intended to be set with values from other modules (i.e. end-user values)
-  #
-  #
-  # 1. Remove nulls from overrideMOdules config
+
+  # Create override config that is intended to be set with values from consumers of this profile
+  # - Remove nulls from overrideMOdules config, so that we don't override modules with null
+  # - If a value is null or empty, it means the consumer did not set an override. Hence, we
+  #   do not need to use the null or empty value to override the modules.
   overrideModulesCfgWithoutNulls = dmUtils.removeNullsAndEmptySets cfg.overrideModules;
   overrideModulesCfgWithPriorities = dmUtils.mkPreset consumerCfgPriority overrideModulesCfgWithoutNulls;
-  # overrideModulesCfg = dmUtils.mkPreset consumerCfgPriority cfg.overrideModules;
 in
 {
-  # Re-use the option definitions from "modules.android"
-  # update: this doesn't work
-  # imports = [
-  #   (lib.mkAliasOptionModule
-  #     [ "devmods" "profiles" "android-dev-env" "android" ]
-  #     [ "devmods" "modules" "android" ]
-  #   )
-  # ];
-  #
   options.devmods.profiles.android-dev-env = {
     enable = lib.mkEnableOption "Enable the Android development environment";
     presets = lib.mkOption {
@@ -77,172 +73,26 @@ in
       '';
     };
 
-    # android = lib.mkOption {
-    #   type = lib.types.submodule (import ../modules/android/default.nix);
-    #   default = { };
-    #   description = "Android-specific configuration.";
-    # };
-
-    # android = lib.mkOption {
-    #   type = lib.types.attrs;
-    #   default = { };
-    #   description = "Override android module's `settings` options.";
-    # };
-
-    # TODO
-    # remove the "enable" option, don't allow user to set this
-    # TODO
-    # use the `options.devmods.modules.android` value, instead of explicit import;
-
-    # Pass in java and gradle version if need to override
-    # java = (import ../modules/languages/java/options.nix { inherit lib; });
-    #
-    # languages.java.version = lib.mkOption {
-    #   # type = lib.types.nullOr dmTypes.version;
-    #   type = lib.types.nullOr lib.types.str;
-    #   default = null;
-    #   description = ''
-    #     The Java package (JDK version) to use. You can specify versions that exist in nixpkgs.
-    #     e.g. 17, 21, 23
-    #     If multiple versions are specified in the configuration, because we're using `dmTypes.version`, we
-    #     will take the highest version of all the conflicting values.
-    #     If no value is specified, we will take the default `jdk` package from nixpkgs.
-    #     If value is an empty string, behavior is undefined.
-    #   '';
-    # };
-    # };
+    # Take existing modules' options and make a clone of them, with the addition
+    # of making their type "nullable", with a default value of null.
+    # This allows us to determine wheether an override value was specified or not
+    # by the consumer.
     overrideModules = {
-      # TODO add null to the options
-      # android = import ../modules/android/options.nix { inherit lib dmTypes; };
-      # android = dmUtils.makeNullableOptionsRecursive options.devmods.modules.android;
-      # TODO test below code. if it works, replace above with it
-      # android = import options.devmods.modules.android;
+      android = dmUtils.makeNullableOptionsRecursive options.devmods.modules.android;
       languages.java = dmUtils.makeNullableOptionsRecursive options.devmods.modules.languages.java;
       gradle = dmUtils.makeNullableOptionsRecursive options.devmods.modules.gradle;
     };
-
   };
 
   config = lib.mkIf cfg.enable {
-    # TODO make presets override the versions? or the individual versions override presets?
-    # I think we want latter, so we should manually merge the `android-dev-env.android`
-    # options, with pressets?
-
-    # devmods.profiles.android-dev-env.androide = true;
-    #
-    #
-    # TODO
-    # can we override
-
-    # TODO figure out if we want to use null, or "default"
-    # devmods.profiles.android-dev-env.java.version = lib.mkDefault null;
-    # devmods.profiles.android-dev-env.java.version =
-    #   if config.devmods.profiles.android-dev-env.java.version == null then
-    #     # if preset available use it
-    #     "17"
-    #   else
-    #     null;
-    # # else pas in null or "latest"?
-    # else
-    #   # use default (or user config value?)
-    #   config.devmods.profiles.android-dev-env.java.version;
-
-    # Override devmods.profiles.java with presets
     devmods.modules = lib.mkMerge (
-      # Add option values from presets
+      # Override module option values from presets
       selectedPresetsList
       ++ [
-        # Add base preset option values
+        # Override module option values with  base preset option values
         basePreset
-        # Add overrides
-        # overrideModulesCfg
+        # Override module option values with consumer-specified overrides
         overrideModulesCfgWithPriorities
-        # {
-        #   android = {
-        #     platform.compileSdkVersion = "from override 4";
-        #   };
-        # }
-
-        # {
-        #   # gradle = {
-        #   #   enable = true;
-        #   #   version = lib.optionalAttrs (lib.hasAttrByPath [ "gradle" "version" ] cfg) cfg.gradle.version;
-        #   # };
-
-        #   languages.java = {
-        #     enable = true;
-        #     # lib.mkOverride 70 cfg.java.version;
-
-        #     # How do i make sure above changes priority for user-set value, not default value?
-        #     # Problem is, cfg.java.version is both the user value, and the default option value.
-        #     # Oh what if we don't set a default?
-        #   };
-
-        # }
-
-        # # {
-        # #   languages.java.version = lib.optionalAttrs (lib.hasAttrByPath [
-        # #     "java"
-        # #     "version"
-        # #   ] cfg) (lib.mkOverride 100 cfg.java.version);
-        # # }
-        # # User overrides
-        # {
-        #   languages.java.version =
-        #     let
-        #       preset_exists = true;
-        #     in
-        #     lib.optionalAttrs
-        #       (lib.hasAttrByPath [
-        #         "java"
-        #         "version"
-        #       ] cfg)
-        #       (
-        #         if cfg.java.version == null && preset_exists then
-        #           "17" # placeholder for preset
-        #         else
-        #           cfg.java.version
-        #         # lib.mkOverride 100 cfg.java.version
-        #       );
-
-        # }
-        # {
-        #   android = (lib.attrByPath [ "android" ] { } cfg);
-        #   # android = {
-        #   #   platform.compileSdkVersion = "from manual override";
-        #   # };
-        #   # Always enable android since this android-dev-env profile is enabled
-        #   # No need for `lib.recursiveUpdate`, since we are not merging nested attributes
-        #   # android = (lib.attrByPath [ "android" ] { } cfg) // {
-        #   #   enable = true;
-        #   # };
-        # }
-
-        # { android.platform.compileSdkVersion = lib.mkOverride 10 cfg.android.platform.compileSdkVersion; }
-        # { android.platform.compileSdkVersion = lib.mkForce "manual update"; }
-        # // {
-        #   platform.compileSdkVersion = cfg.android.platform.compileSdkVersion;
-        #   platform.versions = [ "34" ];
-        #   androidGradlePlugin.version = "8.6.0";
-        #   buildTools.versions = [ "34.0.0" ];
-        #   cmake.versions = [ "3.22.1" ];
-        #   ndk.versions = [
-        #     "26.3.11579264"
-        #   ];
-        #   systemImageTypes = [
-        #     "google_apis"
-        #     "google_apis_playstore"
-        #   ];
-        #   abis = [
-        #     "armeabi-v7a"
-        #     "arm64-v8a"
-        #   ];
-        #   ndk.enable = true;
-
-        # }
-        # // {
-        # platform.compileSdkVersion = lib.mkOverride 10 "100001";
-        # }
       ]
     );
   };
